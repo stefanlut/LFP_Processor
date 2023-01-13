d49e0551c155b79dff28b677e68973f44686c8bb
Stefan Lutschg <hirzli2@gmail.com>
2023-01-12 22:29:21 -0500

    Fun fixes

--- LFP_Processor.py
+++ LFP_Processor.py
@@ -1,14 +1,19 @@
 import sys
+
 import matplotlib
 from pylab import *
-matplotlib.use('Qt5Agg')
-import scipy.io as sio
 from scipy import signal
-from PyQt5 import QtCore, QtGui, QtWidgets
 import numpy as np
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
+import scipy.io as sio
+
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import \
+    NavigationToolbar2QT as NavigationToolbar
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Window(QtWidgets.QDialog):
     def __init__(self, parent=None):
@@ -32,54 +37,84 @@ class Window(QtWidgets.QDialog):
                 self.toolbar,
                 self.canvas,
                     ]
-        pushplot_widget = QtWidgets.QPushButton('Plot')
-        pushplot_widget.clicked.connect(lambda: self.plot(self.load_LFP_data()))
+        pushplot_widget = QtWidgets.QPushButton('Load and Plot LFP Data')
+        pushplot_widget.clicked.connect(lambda: self.plot(self.load_LFP_data(),dropdown_widget.currentIndex(),t1_widget.text(),t2_widget.text()))
         dropdown_widget = QtWidgets.QComboBox()
         dropdown_widget.addItems(['Delta','Theta','Alpha','Beta','Gamma'])
-        pushselect_widget = QtWidgets.QPushButton('Load LFP Data')
-        pushselect_widget.clicked.connect(self.load_LFP_data)
-        
-        
-        # Just some button connected to 'plot' method
-        #self.button = QtWidgets.QPushButton('Plot')
+        t1_widget = QtWidgets.QLineEdit()
+        t2_widget = QtWidgets.QLineEdit()
+        t1_widget.setPlaceholderText("Enter t1 of interval [t1, t2] as a decimal")
+        t2_widget.setPlaceholderText("Enter t2 of interval [t1, t2] as a decimal")
         for w in widgets:
             layout.addWidget(w)
         layout.addWidget(dropdown_widget)
         layout.addWidget(pushplot_widget)
-        layout.addWidget(pushselect_widget)
+        layout.addWidget(t1_widget)
+        layout.addWidget(t2_widget)
+        # layout.addWidget(pushselect_widget)
         # setting layout to the main window
         self.setLayout(layout)
 
     def load_LFP_data(self):
         dlg = QtWidgets.QFileDialog()
         dlg.setFileMode(QtWidgets.QFileDialog.AnyFile)
-        dlg.setNameFilter("MATLAB Data (*.mat)")
+        dlg.setNameFilter("MATLAB Workspace Data (*.mat)")
         filenames = list()
-        matname = ""
+        matname = None
         if dlg.exec_():
             filenames = dlg.selectedFiles()
             matname = filenames[0]
+        
         return matname
         
  # action called by the push button
-    def plot(self,fname = None):
-          
+    def plot(self,fname = None, bandIndex=None, t1 = None,t2 = None):
+        if fname == None:
+            dlg = QtWidgets.QMessageBox()
+            dlg.setWindowTitle("Error")
+            dlg.setText("No File Selected")
+            button = dlg.exec()
+            return
+        if bandIndex == 0:
+            Wn = [0.5,4]
+        elif bandIndex == 1:
+            Wn = [4,8]
+        elif bandIndex == 2:
+            Wn = [8,12]
+        elif bandIndex == 3:
+            Wn = [12,35]
+        elif bandIndex == 4:
+            Wn = [35, 120]
         # LFP data
         data = sio.loadmat(fname)
-        LFP = data['LFP'][0]
+        LFP = data['LFP'][0] - np.mean(data['LFP'][0])
         t = data['t'][0]
-        t1 = np.where(t==44.0)[0][0]
-        t2 = np.where(t==47.0)[0][0]
+        try:
+            if t1 == "" or t2 == "":
+                dlg = QtWidgets.QMessageBox()
+                dlg.setWindowTitle("Error")
+                dlg.setText("No Time Interval Requested")
+                button = dlg.exec()
+                return
+            t1 = np.where(t== float(t1) )[0][0]
+            t2 = np.where(t==float(t2) )[0][0]
+        except:
+            dlg = QtWidgets.QMessageBox()
+            dlg.setWindowTitle("Error")
+            dlg.setText("Invalid Time Interval Requested")
+            button = dlg.exec()
+            return
+            
         dt = t[1] - t[0]                     # Define the sampling interval,
-        fNQ = 1 / dt / 2                     # ... and Nyquist frequency.
-        Wn = [80, 120];                      # Set the passband [80-120] Hz,
+        Fs = 1/dt                       # Define sampling rate
+        fNQ = Fs/ 2                     # ... and Nyquist frequency.
+        #Wn = [80, 120];                      # Set the passband [80-120] Hz,
         n = 100;                             # ... and filter order,
         b = signal.firwin(n, Wn, nyq=fNQ, pass_zero=False, window='hamming')
-        Vhi = signal.filtfilt(b, 1, LFP-np.mean(LFP))    # ... and apply it to the data.
-        Vlo = signal.filtfilt(signal.firwin(n,[4,7],nyq = fNQ, pass_zero = False, window='hamming'),1,LFP - np.mean(LFP))
-        X_Gamma = signal.hilbert(Vhi)
-        phi = angle(signal.hilbert(Vlo))     # Compute phase of low-freq signal
-        amp = abs(X_Gamma)       # Compute amplitude of high-freq signal
+        Vhi = signal.filtfilt(b, 1, LFP)    # ... and apply it to the data.
+        X = signal.hilbert(Vhi)
+        phi = angle(X)     # Compute phase of low-freq signal
+        amp = abs(X)       # Compute amplitude of high-freq signal
         # clearing old figure
         self.figure.clear()
   
@@ -90,9 +125,13 @@ class Window(QtWidgets.QDialog):
         ax3.set_xlabel("Time (s)")
         # plot data
         ax1.plot(t[t1:t2],LFP[t1:t2])
+        ax1.set_ylabel('Voltage')
+        ax1.get_xaxis().set_visible(False)
         ax2.plot(t[t1:t2],amp[t1:t2])
+        ax2.get_xaxis().set_visible(False)
         ax2.set_ylabel('Amplitude')
         ax3.plot(t[t1:t2],phi[t1:t2])
+        ax3.set_ylabel('Phase (rad)')
         # refresh canvas
         self.canvas.draw() 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LFP_Processor
-A Graphical User Interface built on Python and QT framework that can process Local Field potential data
+A Graphical User Interface built on Python and QT framework that can process Local Field Potential data.
+
+The ideal file format to use with this is a MATLAB .mat file with two variables:
+LFP - LFP Data
+t - time data
